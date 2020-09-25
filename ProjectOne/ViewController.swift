e1117f9252189b5e74793b887b3c45ccf72cfe97
Arie Peretz <ariep20@gmail.com>
2020-09-25 09:38:50 +0300

    Initial Commit

---
+++ ProjectOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectOne
+//
+//  Created by Arie Peretz on 25/09/2020.
+//  Copyright © 2020 Arie Peretz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
